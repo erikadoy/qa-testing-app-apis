599f677ed3a3e992f7fcc25a6a9ca7873e143277
erikadoy <erikad@hotmail.co.uk>
2026-03-22 17:09:43 +0000

    updating more tests

---
+++ tests/post_test.py
@@ -0,0 +1,27 @@
+import pytest
+import requests
+import uuid
+
+ENDPOINT = "https://bookish-waffle-9vp65q79g7jh955v-8000.app.github.dev"
+
+def test_create_project():
+    unique_team_name = f"TestProject-{uuid.uuid4()}" # I want to create a unique team each time, so when it runs again, it doesnt come back with a 400 because its already created.
+
+    payload = {
+        "project_name": unique_team_name,
+        "team_name": "Platform Engineering",
+        "status": "active",
+        "templates": ["frontend-react"]
+    }
+
+    response = requests.post(f"{ENDPOINT}/api/v1/projects", json=payload)
+
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["project_name"] == unique_team_name
+    assert data["team_name"] == "Platform Engineering"
+    assert data["status"] == "active"
+    assert data["templates"] == ["frontend-react"]
+    assert "project_id" in data
